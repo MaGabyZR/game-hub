50cd2fdafa588ccfadc96879c20162dceb77f852
Gabriela Rubio <gabyrubioa@gmail.com>
2024-08-08 11:37:16 -0600

    Create custom hook to fetch the games.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-//to represent the games
-interface Game {
-  id: number;
-  name: string;
-}
-
-//to represent and match the properties from rawg.io
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 export const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  //send the request to the backend, to the /games endpoint.
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+//this is a custom hook for fetching the games and making the HTTP requests.
+
+
+//to represent the games
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  //to represent and match the properties from rawg.io
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+const useGames = () =>{
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+
+  //send the request to the backend, to the /games endpoint.
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+    });
+
+      return () => controller.abort(); //cleanup function. 
+
+  },[]); //[] to avoid and endless loop of calls to the backend.
+
+  return { games, error };
+
+}
+
+export default useGames;
