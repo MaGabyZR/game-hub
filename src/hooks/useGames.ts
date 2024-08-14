7558e22cfa079e0add4e6755b034d2becfb5a05f
Gabriela Rubio <gabyrubioa@gmail.com>
2024-08-13 19:07:31 -0600

    Display platform icons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,12 +3,21 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 //this is a custom hook for fetching the games and making the HTTP requests.
 
+//To display the platform icons.
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 
 //to represent the games, they are all properties that comer from the list of game from rawg.io
 export interface Game {
     id: number;
     name: string;
     background_image: string; 
+    parent_platforms: { platform: Platform }[]
+
   }
   
   //to represent and match the properties from rawg.io
