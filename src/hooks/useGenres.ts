770bae18f0bd1bed932be604cb78c8e390e0b163
Gabriela Rubio <gabyrubioa@gmail.com>
2024-08-19 11:36:30 -0600

    Display genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -5,6 +5,7 @@ import useData from "./useData";
 export interface Genre {
     id: number;
     name: string;
+    image_background: string;
 }
 //use the generic hook to call the backend
 const useGenres = () => useData<Genre>('/genres');
