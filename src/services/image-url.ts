8559f9b67b2db7217852c672cd7b6611d4f6c439
Gabriela Rubio <gabyrubioa@gmail.com>
2024-08-22 12:23:58 -0600

    Handle the games without an image.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,12 @@
+import noImage from '../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp'; 
+
 
 const getCroppedImageUrl = (url:string) => {
-    if (!url) return ''; //temporary solution for game that don´t have an image.
+    //if (!url) return ''; //temporary solution for game that don´t have an image.
+    //import an blanck image to handle the games with no image. 
+    if (!url) return noImage;
+
+
 
     const target = 'media/';
     const index = url.indexOf(target) + target.length; 
